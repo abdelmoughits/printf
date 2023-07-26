6eb4b99c2fc427aefd15e6436c2deb7e97fba2b6
Abdelmoughit <thepassengernumberfive7@gmail.com>
2023-07-26 11:45:08 -0700

    update

--- _printf.c
+++ _printf.c
@@ -91,6 +91,10 @@ int _printf(const char *format, ...)
 					i++;
 					break;
 				default:
+					if (format[i + 1] = '\0')
+					{
+						return (-1);
+					}
 					write(1, &(format[i]), 1);
 					b++;
 			}
